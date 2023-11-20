d53704b38dfdedaa380ccc9b9e8de58c324de982
Aravind Arulmoli Brindha Parthasarathy <aravind.parthasarathy.26.1@mountblue.tech>
2023-11-20 12:03:05 +0530

    Initial commit

---
+++ src/main/java/io/mountblue/c26_1java/aravind/blogapplication/BlogApplication.java
@@ -0,0 +1,13 @@
+package io.mountblue.c26_1java.aravind.blogapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mountblue/c26_1java/aravind/blogapplication/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mountblue.c26_1java.aravind.blogapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
