223a02c291ea57be442ff6cae936033356e1a449
Aravind Arulmoli Brindha Parthasarathy <aravind.parthasarathy.26.1@mountblue.tech>
2023-11-21 16:03:44 +0530

    Add functionality for reading a full post

--- src/main/java/io/mountblue/c26_1java/aravind/blogapplication/controller/PostController.java
+++ src/main/java/io/mountblue/c26_1java/aravind/blogapplication/controller/PostController.java
@@ -5,10 +5,7 @@ import io.mountblue.c26_1java.aravind.blogapplication.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -46,5 +43,14 @@ public class PostController {
         postService.save(post);
 
         return "redirect:/blog-application/";
+    }
+
+    @GetMapping("/showpost")
+    public String showPost(@RequestParam Long id, Model model) {
+        Post post = postService.findById(id);
+
+        model.addAttribute("post", post);
+
+        return "view-post";
     }
 }
